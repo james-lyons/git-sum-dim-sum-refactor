51003d1d3897173681ccf795ccc8f6081d694cb6
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-01-16 15:57:30 -0800

    5. Sets up basic file structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    };
+};
+
+export default App;
